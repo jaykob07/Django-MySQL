9c102db276d91e9a5e1abbeb19d473bf618c1150
jacobo <jaykob_07@hotmail.com>
2023-12-22 21:27:56 -0500

    correccion model

--- dgmsl/website/forms.py
+++ dgmsl/website/forms.py
@@ -16,7 +16,7 @@ class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget.attrs['class'] = 'form-control bg-secondary'
+        self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
         self.fields['username'].label = ''
         self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
@@ -30,3 +30,5 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+    
--- dgmsl/website/views.py
+++ dgmsl/website/views.py
@@ -2,24 +2,25 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
-
+from .models import Record
 # Create your views here.
 def home(request):
-	# revision del usario para ingresar de manera correcta
+	records = Record.objects.all()
+	# Check to see if logging in
 	if request.method == 'POST':
 		username = request.POST['username']
 		password = request.POST['password']
-		# Autenticacion
+		# Authenticate
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
 			login(request, user)
-			messages.success(request, "Login con exito!")
+			messages.success(request, "You Have Been Logged In!")
 			return redirect('home')
 		else:
-			messages.success(request, "Algo salio mal, revisa tu usuario o tu password")
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
 			return redirect('home')
 	else:
-		return render(request, 'home.html', {})
+		return render(request, 'home.html', {'records':records})
 
 
 
@@ -35,7 +36,7 @@ def register_user(request):
 		form = SignUpForm(request.POST)
 		if form.is_valid():
 			form.save()
-			# Authenticate and login
+			# Autenticacion y login 
 			username = form.cleaned_data['username']
 			password = form.cleaned_data['password1']
 			user = authenticate(username=username, password=password)
@@ -46,7 +47,16 @@ def register_user(request):
 		form = SignUpForm()
 		return render(request, 'register.html', {'form':form})
 
+	return render(request, 'register.html', {'form':form})
 	
 
 
 
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Look Up Record
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "Debes ser logeado para ingresar")
+		return redirect('home')
